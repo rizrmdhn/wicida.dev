0fa346310f8c2488db9022c5256e2bbd55a1c6f3
rizrmdhn <rizrmdhn.work@gmail.com>
2023-12-01 11:40:34 +0800

    moved main section to seperated files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,25 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: "#772E71",
-        secondary: "#FAFAFA",
-        accent: "#0F0F0F",
-      },
-      height: {
-        160: "160%",
-      },
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#772E71',
+                secondary: '#FAFAFA',
+                accent: '#0F0F0F',
+            },
+            height: {
+                135: '135%',
+                160: '160%',
+            },
+            width: {
+                44: '44%',
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
